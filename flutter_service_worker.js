6ff8608522a46722262c6f137e0f03ac5d77c8bf
elberfeld2 <elberfeld2@users.noreply.github.com>
2020-06-22 03:01:14 +0000

    deploy: a0df90f9d9b5c5d255211cc986733c4604f47b9f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon_2.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "ae284210431f7fb9d37a293d3075f271",
+"main.dart.js": "0a78a14cb07c92cc3a0717c458f8770c",
 "assets/NOTICES": "108d0a1640e6b622b04c79e5a1de9f51",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
